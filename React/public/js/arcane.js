68cb5ad7ff0e1a74c007e2012c68f32830fd32b7
Rhuarc <ssedrick1@gmail.com>
2016-06-15 00:53:47 -0600

    Got the player to play, pause, restart, and seek

--- React/public/js/arcane.js
+++ React/public/js/arcane.js
@@ -1,32 +1,67 @@
-var isMenuOpen = false;
 var audioFileType = "";
+var isPlaying = false;
+var isPaused = false;
 
-function closeNav() {
-   document.getElementById("mySidenav").style.width = "0";
-   document.getElementById("main").style.marginLeft = "0";
-}
-
-function openNav() {
-   document.getElementById("mySidenav").style.width = "250px";
-   document.getElementById("main").style.marginLeft = "250px";
-}
-
-function controlNav() {
-   if (!isMenuOpen) {
-     openNav();
-     isMenuOpen = true;
-   } else {
-     closeNav();
-     isMenuOpen = false;
-   }
-}
+var play     = $('#play');
+var restart  = $('#previous');
+var next     = $('#next');
+var progress = document.getElementById('progressBar');
+var song;
 
 function setUpPlayer() {
    song = new Audio();
    if (song.canPlayType('audio/mpeg;')) {
       audioFileType = ".mp3";
+      song.type = 'audio/mpeg';
    } else {
       audioFileType = ".ogg";
+      song.type = 'audio/ogg';
    }
 
 }
+
+$(document).ready( function() {
+   setUpPlayer();
+
+   play.click( function(e) {
+      e.preventDefault();
+      if (!isPlaying) {
+         if (!isPaused) {
+            song.src = "/home/perrin/Music/iris" + audioFileType;
+         }
+         song.play();
+
+         $(this).children('i').replaceWith('<i class="material-icons">pause</i>');
+         $('#progressBar').attr('max', song.duration);
+         isPlaying = true;
+      } else {
+         song.pause();
+         $(this).children('i').replaceWith('<i class="material-icons">play_arrow<i>');
+         isPlaying = false;
+         curtime = parseInt(song.currentTime, 10);
+         progress.MaterialSlider.change(curtime);
+         isPaused = true;
+      }
+   });
+
+   $("#progressBar").bind("change", function() {
+      song.currentTime = $(this).val();
+      $("#progressBar").attr("max", song.duration);
+   });
+
+   song.ontimeupdate = function() {
+      curtime = parseInt(song.currentTime, 10);
+      progress.MaterialSlider.change(curtime);
+   };
+
+   restart.click( function(e) {
+      e.preventDefault();
+      progress.MaterialSlider.change(0);
+      song.currentTime = 0;
+   });
+
+   next.click( function(e) {
+      e.preventDefault();
+      alert("We are currently working on this functionality. Please be patient as we continue to update our program. Thanks!\nArcane Development Team");
+   });
+});
