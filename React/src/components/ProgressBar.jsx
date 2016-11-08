4c505feeccdfeab9064efc2223805905cbb79978
Shem Sedrick <ssedrick1@gmail.com>
2016-11-08 15:24:53 -0700

    Got rid of som extra code

--- React/src/components/ProgressBar.jsx
+++ React/src/components/ProgressBar.jsx
@@ -3,7 +3,6 @@ import Slider from 'material-ui/Slider';
 
 export default class ProgressBar extends React.Component {
    render() {
-      //return <input onChange={this.props.onChange} className="mdl-slider mdl-js-slider" id="progressBar" type="range" min="0" max="100" defaultValue="0" tabIndex="0" />
       return (<Slider onChange={this.props.onChange} value={0} />);
    }
 }
